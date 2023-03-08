e91b8dd655d86b4888902312a433a60482b1b79f
Juan Venegas <j.venegasgutierrez@gmail.com>
2023-03-08 12:17:32 -0300

    se actualizan datos para quirofanos

--- app_salud/app/logic/utilizacion_de_quirofanos/tabla.R
+++ app_salud/app/logic/utilizacion_de_quirofanos/tabla.R
@@ -29,10 +29,10 @@ server <- function(id) {
     
     output$tabla<-reactable$renderReactable({
       
-      xlsx::read.xlsx(file="app/logic/data/set_de_datos.xlsx",sheetIndex = 2, rowIndex = 1:12, colIndex= 1:11
+      xlsx::read.xlsx(file="app/logic/data/set_de_datos_1.xlsx",sheetIndex = 4, rowIndex = 1:12, colIndex= 1:12
                       , as.data.frame = TRUE, header = TRUE) |>
         dplyr::mutate_if(is.numeric, ~ dplyr::case_when(. < 2 ~ round(., 2), TRUE ~ ceiling(.))) |> 
-        dplyr::mutate_at(7:11, scales::percent) |>
+        dplyr::mutate_at(8:12, scales::percent) |>
         reactable::reactable(searchable = TRUE, minRows = 10) 
           
     })
