64919921fef3f27be60880474d98dbae4ecad576
AnastasiaRebolledo <anastasia.rebolledo.r@gmail.com>
2023-03-09 13:07:20 -0300

    .

--- app_salud/app/logic/Tiempo_extra/grafico_horizontal.R
+++ app_salud/app/logic/Tiempo_extra/grafico_horizontal.R
@@ -3,7 +3,7 @@
 box::use(
   reactable,
   MASS,
-  shiny[h3, moduleServer, NS, tagList],
+  shiny[h3, moduleServer, NS, tagList,selectInput],
   dplyr,
   utils,
   echarts4r
@@ -15,12 +15,14 @@ ui <- function(id) {
   ns <- NS(id)
   
   tagList(
+    selectInput(ns("selector_2"),"Seleccionar orden",choices = c("Tiempo adicional decreciente"="Tiempo.adicional","Tiempo de inactividad decreciente"="Tiempo.de.inactividad")),
     echarts4r$echarts4rOutput(ns("grafico_horizontal"))
     
   )
   
 }
-
+datos<-xlsx::read.xlsx(file="app/logic/data/set_de_datos_1.xlsx",sheetIndex = 7, rowIndex = 267:272, colIndex= 4:6
+                , as.data.frame = TRUE, header = TRUE)
 
 #' @export
 server <- function(id) {
@@ -28,6 +30,7 @@ server <- function(id) {
     output$grafico_horizontal<- echarts4r$renderEcharts4r({ 
       xlsx::read.xlsx(file="app/logic/data/set_de_datos_1.xlsx",sheetIndex = 7, rowIndex = 267:272, colIndex= 4:6
                       , as.data.frame = TRUE, header = TRUE)|> 
+        #dplyr::arrange(input$selector_2)|>
         dplyr::arrange(Tiempo.adicional)|>
         echarts4r::e_chart(Especialidad) |>
         echarts4r::e_bar(Tiempo.adicional, name = "Tiempo adicional promedio") |>
