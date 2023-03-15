97af8611d2da70cc0b105192e451d4eaf6ad97c1
Juan Venegas <j.venegasgutierrez@gmail.com>
2023-03-15 15:22:40 -0300

    se actualiza conexion

--- app_salud/app/global.R
+++ app_salud/app/global.R
@@ -9,6 +9,6 @@ db_password <- Sys.getenv("DATABASE_PASS")
 pool <- pool::dbPool(drv=RPostgres::Postgres(),
                      host   = "db-postgresql-nyc1-40399-do-user-13749128-0.b.db.ondigitalocean.com",
                      dbname = "defaultdb",
-                     user      = "Anastasia",
+                     user      = "doadmin",
                      password  = db_password,
                      port     = 25060)
