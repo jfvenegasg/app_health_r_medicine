691a088902beebc6a127a22abdcfe6a28b1fe637
Juan Venegas <j.venegasgutierrez@gmail.com>
2023-04-04 14:01:17 -0400

    se actualiza server

--- app-medicine-r/server.R
+++ app-medicine-r/server.R
@@ -204,10 +204,10 @@ susp_esp<-data.frame(openxlsx::read.xlsx(xlsxFile ="modulos/data/datos_supension
       echarts4r::e_tooltip(trigger = "item",axisPointer = list(type = "shadow"))
 })
 
-total<-sum(susp_esp$cantidad)
+#total<-sum(susp_esp$cantidad)
 
 susp_esp<-data.frame(openxlsx::read.xlsx(xlsxFile ="modulos/data/datos_supensiones_por_especialidad.xlsx" ,sheet ="Hoja1" ,rows = 1:73,cols = 13:15 ))
-output$grafico_circular1<- echarts4r$renderEcharts4r({ 
+output$grafico_circular1<- renderEcharts4r({ 
   aggregate(cantidad ~ Tipo, data=susp_esp,FUN = sum) |> 
     echarts4r::e_chart(Tipo) |>
     echarts4r::e_pie(cantidad, radius = c("40%", "70%")) |>
@@ -218,7 +218,7 @@ output$grafico_circular1<- echarts4r$renderEcharts4r({
     echarts4r::e_tooltip(trigger = "item",axisPointer = list(type = "shadow"))
 })
 
-output$grafico_circular2<- echarts4r$renderEcharts4r({ 
+output$grafico_circular2<- renderEcharts4r({ 
   aggregate(cantidad ~ Especialidad, data=susp_esp,FUN = sum) |> 
     dplyr::arrange(Especialidad)|>
     echarts4r::e_chart(Especialidad) |>
